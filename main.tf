9745bdde66a2fdb92072c5085e247368b8db7c1b
Dr. Brent Hollers <72216475+brent-hollers@users.noreply.github.com>
2026-03-09 22:12:46 -0400

    Fix: updated userdata template file and provider, successful code deploy

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 terraform {
   required_providers{
     aws = {
-        source = "hashicorp/aws"
-        region = "us-east-1"
+        source = "hashicorp/aws" 
     }
-    tags = common_tags
   }
     
     }
+
+provider "aws"{
+  region = "us-east-1"
+
+}
 
 resource "aws_lightsail_instance" "dns_server" {
   name              = "example"
   availability_zone = "us-east-1b"
   blueprint_id      = "ubuntu_22_04"
   bundle_id         = "micro_3_0"
-  user_data         = templatefile("${path.module}/scripts/user_data.sh")
+  user_data         = templatefile("${path.module}/scripts/userdata.sh", {datadog_api_key = var.datadog_api_key})
 
   lifecycle {
     prevent_destroy = true
   }
-}
-
-resource "aws_lightsail_vpc_peering" "dns_peering" {
-  enabled = true
+  tags = local.common_tags
 }
