3eabca423df685b53403189f74bb7205f2c8ecf7
Brent Hollers <coachhollers@gmail.com>
2026-03-20 09:02:36 -0400

    fix: updated userdata to allow traffic through multiple networks, added SSL certs for bidirectional encrypted DNS, migrated to EC2 instance instead of lightsail to allow for appropriate VPN communication

--- main.tf
+++ main.tf
@@ -11,16 +11,185 @@ provider "aws"{
   region = "us-east-1"
 
 }
+data "aws_ami" "Ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = [ "099720109477" ]
+}
 
-resource "aws_lightsail_instance" "dns_server" {
-  name              = "example"
+resource "aws_instance" "dns_server" {
   availability_zone = "us-east-1b"
-  blueprint_id      = "ubuntu_22_04"
-  bundle_id         = "micro_3_0"
+  instance_type     = "t4g.micro"
+  ami               = data.aws_ami.Ubuntu.id
+  security_groups   = [aws_security_group.dns_sg.name]
+  iam_instance_profile = aws_iam_instance_profile.dns_instance_profile.name
   user_data         = templatefile("${path.module}/scripts/userdata.sh", {datadog_api_key = var.datadog_api_key})
-
+  key_name = "ssh_dns_instance"
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
   tags = local.common_tags
+}
+
+resource "aws_security_group" "dns_sg" {
+  name        = "dns-server-sg"
+  description = "Allow DNS traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["10.21.0.0/16"]
+  }
+
+    ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = ["10.21.0.0/16"]
+  }
+
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.21.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "dns_role" {
+  name = "dns-server-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ssm.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "dns_policy" {
+  name        = "dns-server-policy"
+  description = "Policy for DNS server to allow SSM access"
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "CloudWatch",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:DescribeAlarmHistory",
+                "cloudwatch:DescribeAlarms",
+                "cloudwatch:GetMetricData",
+                "cloudwatch:ListMetrics",
+                "cloudwatch:PutMetricAlarm",
+                "cloudwatch:DeleteAlarms",
+                "cloudwatch:PutAnomalyDetector",
+                "cloudwatch:DeleteAnomalyDetector",
+                "cloudwatch:DescribeAnomalyDetectors"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Sid": "CloudWatchLogs",
+            "Effect": "Allow",
+            "Action": [
+                "logs:FilterLogEvents",
+                "logs:GetLogEvents",
+                "logs:DescribeLogStreams",
+                "logs:DescribeLogGroups",
+		"logs:CreateLogGroup",
+		"logs:CreateLogStream",
+		"logs:PutLogEvents"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+
+        {
+            "Sid": "EC2",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstances",
+                "ec2:DescribeVolumes",
+                "ec2:DescribeVolumeStatus",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeVpcAttribute",
+                "ec2:DescribeNatGateways"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+
+        {
+            "Sid": "SSM",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeInstanceInformation",
+                "ssm:GetCommandInvocation",
+                "ssm:ListCommands",
+                "ssm:SendCommand"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+        ]
+      })
+  } 
+
+resource "aws_iam_role_policy_attachment" "dns_policy_attachment" {
+  role       = aws_iam_role.dns_role.name
+  policy_arn = aws_iam_policy.dns_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "dns_role_attachment" {
+  role       = aws_iam_role.dns_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "dns_instance_profile" {
+  name = "dns-server-instance-profile"
+  role = aws_iam_role.dns_role.name
+}
+
+resource "aws_eip" "dns_eip" {
+  instance = aws_instance.dns_server.id
+  domain = "vpc"
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,61 +1,59 @@
-user_data = <<-EOF
-  #!/bin/bash
-  apt-get update -y
-  apt-get upgrade -y
-  apt-get install -y unbound
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y
 
-  cat > /etc/unbound/unbound.conf <<-UNBOUND
-  server:
-      interface: 0.0.0.0
-      port: 53
-      do-ip4: yes
-      do-ip6: no
-      do-tcp: yes
-      do-udp: yes
+systemctl stop systemd-resolved
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+echo "$${PRIVATE_IP} $${HOSTNAME}" >> /etc/hosts
 
-      access-control: 127.0.0.0/8 allow
-      access-control: 10.21.0.0/16 allow
-      access-control: 0.0.0.0/0 refuse
+apt-get install -y unbound
 
-      cache-min-ttl: 300
-      cache-max-ttl: 86400
-      msg-cache-size: 64m
-      rrset-cache-size: 128m
+cat > /etc/unbound/unbound.conf <<UNBOUND
+server:
+    interface: 0.0.0.0
+    port: 53
+    do-ip4: yes
+    do-ip6: no
+    do-tcp: yes
+    do-udp: yes
 
-      prefetch: yes
-      prefetch-key: yes
+    access-control: 127.0.0.0/8 allow
+    access-control: 10.21.0.0/16 allow
+    access-control: 10.21.0.0/16 allow
+    access-control: 0.0.0.0/0 refuse
 
-      num-threads: 2
-      so-rcvbuf: 1m
+    cache-min-ttl: 300
+    cache-max-ttl: 86400
+    msg-cache-size: 64m
+    rrset-cache-size: 128m
 
-      auto-trust-anchor-file: "/var/lib/unbound/root.key"
+    prefetch: yes
+    prefetch-key: yes
 
-      hide-identity: yes
-      hide-version: yes
+    num-threads: 2
+    so-rcvbuf: 1m
 
-      verbosity: 1
-      log-queries: no
+    auto-trust-anchor-file: "/var/lib/unbound/root.key"
 
-  forward-zone:
-      name: "."
-      forward-addr: 1.1.1.1@853#cloudflare-dns.com
-      forward-addr: 8.8.8.8@853#dns.google
-      forward-tls-upstream: yes
-  UNBOUND
+    hide-identity: yes
+    hide-version: yes
 
-  systemctl enable unbound
-  systemctl restart unbound
-  
-    DD_API_KEY= ${datadog_api_key}\
-    DD_SITE="us5.datadoghq.com" \
-    DD_APM_INSTRUMENTATION_ENABLED=host \
-    DD_ENV=prod \
-    DD_APM_INSTRUMENTATION_LIBRARIES=java:1,python:4,js:5,php:1,dotnet:3,ruby:2 \
-    DD_RUM_ENABLED=true \
-    DD_RUM_APPLICATION_ID=15c3ba00-1da3-4832-81ea-a2216d1f2561 \
-    DD_RUM_CLIENT_TOKEN=pub628f90b6045fe9ca912612173efac0ec \
-    DD_RUM_REMOTE_CONFIGURATION_ID=cacd3b22-913c-4cca-8894-e2c69fb2f25d \
-    DD_RUM_SITE=us5.datadoghq.com \
-    bash -c "$(curl -L https://install.datadoghq.com/scripts/install_script_agent7.sh)"
+    verbosity: 1
+    log-queries: no
+    tls-cert-bundle: "/etc/ssl/certs/ca-certificates.crt"
 
-EOF
+forward-zone:
+    name: "."
+    forward-addr: 1.1.1.1@853#cloudflare-dns.com
+    forward-addr: 8.8.8.8@853#dns.google
+    forward-tls-upstream: yes
+UNBOUND
+
+systemctl enable unbound
+systemctl restart unbound
+
+DD_API_KEY=${datadog_api_key} \
+DD_SITE="us5.datadoghq.com" \
+bash -c "$(curl -L https://install.datadoghq.com/scripts/install_script_agent7.sh)"
+
